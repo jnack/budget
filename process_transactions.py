8ba7ae2f726a84534f0be18986a808f615d559df
jnack <jason.j.nack@gmail.com>
2017-08-25 07:59:51 -0400

    Initial commit

---
+++ process_transactions.py
@@ -0,0 +1,16 @@
+import sqlite3
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+def import_files(date):
+
+	
+
+def clean_transactions(df, bank):
+
+
+
+def import_transactions(df):
+
