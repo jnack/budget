1bcdafbe1da06cd9040e8c6a69d7e486b416d606
jnack <jason.j.nack@gmail.com>
2017-09-23 13:02:24 -0400

    Date formatting.

--- process_transactions.py
+++ process_transactions.py
@@ -26,10 +26,10 @@ account_dict = {
 # }
 
 wf_trans_types = {
-    'PURCHASE AUTHORIZED' : lambda df: split_wf_purchase(df),
-    'PURCHASE RETURN': lambda df: split_wf_return(df),
+    'PURCHASE AUTHORIZED' : ('Purchase',lambda df: split_wf_purchase(df)),
+    'PURCHASE RETURN': ('Return', lambda df: split_wf_return(df)),
     # 'ATM WITHDRAWAL',
-    'DIRECT DEP': lambda df: split_wf_dd(df),
+    'DIRECT DEP': ('Direct Deposit', lambda df: split_wf_dd(df)),
     # 'eDeposit',
     # 'TRANSFER',
 }
@@ -38,7 +38,7 @@ def import_files(date):
 
     glob_string = '{}*.csv'.format(date)
     import_files = glob.glob(glob_string)
-    # return import_files
+
     for file in import_files:
 
         date, bank, account_type = file[:-4].split('_')
@@ -55,42 +55,43 @@ def import_files(date):
 
         clean_df = clean_transactions(df, bank)
         return clean_df
-        # print(f'Date: {date}\nBank: {bank_dict[bank]}\nAccount Type: {account_dict[account_type]}')
-
 
 def clean_transactions(df, bank):
 
     if bank == 'wf':
 
         df.rename(columns={0: 'record_date', 1: 'amount', 2: 'star', 3: 'blank', 4: 'full_description'}, inplace=True)
-        # print(df.columns)
+        
+        df['record_date'] = pd.to_datetime(df['record_date'])
         
         df_list = []
 
-        for tran_type, tran_str in wf_trans_types.items():
+        for tran_type, tran_tup in wf_trans_types.items():
 
-            sub_df = wf_trans_types[tran_type](
-                df.loc[df['full_description'].str.contains(tran_type)])
-            
-            df_list.append(sub_df)
-            # print(sub_df.head())
+        	df.loc[df['full_description'].str.contains(tran_type), 'transaction_type'] = tran_tup[0]
+
+        	sub_df = tran_tup[1](df.loc[df['full_description'].str.contains(tran_type)])
+
+        	df_list.append(sub_df)
+
         full_df = pd.concat(df_list)
 
         return full_df
 
 def split_wf_purchase(sub_df):
-    sub_df['transaction_type'] = 'Purchase'
-    # sub_df[['transaction_date','formatted_description','transaction_city','transaction_state','source_id_num','card_num']] = sub_df['full_description'].str.extract(r'^PURCHASE AUTHORIZED ON (\d{2}/\d{2}) ([\w#\'\.\-\*]+[\s\w#\'\.\-\*]*) ([A-Za-z]*[\sA-Za-z]*)\s*([A-Za-z]{2}) ([A-Za-z]{1}\d{10,}) CARD (\d{4})$')
-    sub_df[['transaction_date','formatted_description','transaction_city','transaction_state','source_id_num','card_num']] = sub_df['full_description'].str.extract(r'^PURCHASE AUTHORIZED ON (\d{2}/\d{2}) ([\w#\'\.\-\*\\\&\/]+[\s\w#\'\.\-\*\/\\\&]*) ([\w#\'\.\-\*\\\&\/]+[[\s\w\.\/#]{3,4}]*) ([A-Za-z]{2}) ([A-Za-z]{1}\d{10,}) CARD (\d{4})$')
-    return sub_df
+    # sub_df['transaction_type'] = 'Purchase'
+	sub_df[['transaction_date','formatted_description','transaction_city','transaction_state','source_id_num','card_num']] = sub_df['full_description'].str.extract(r'^PURCHASE AUTHORIZED ON (\d{2}/\d{2}) ([\w#\'\.\-\*\\\&\/]+[\s\w#\'\.\-\*\/\\\&]*) ([\w#\'\.\-\*\\\&\/]+[[\s\w\.\/#]{3,4}]*) ([A-Za-z]{2}) ([A-Za-z]{1}\d{10,}) CARD (\d{4})$')
+	sub_df['transaction_date'] = sub_df.apply(format_date, axis=1)
+	return sub_df
 
 def split_wf_return(sub_df):
-    sub_df['transaction_type'] = 'Return'
+    # sub_df['transaction_type'] = 'Return'
     sub_df[['transaction_date','formatted_description','transaction_city','transaction_state','source_id_num','card_num']] = sub_df['full_description'].str.extract(r'^PURCHASE RETURN AUTHORIZED ON (\d{2}/\d{2}) ([\w#\'\.\-\*\\\&\/]+[\s\w#\'\.\-\*\\\/\&]*) ([\w#\'\.\-\*\\\&\/]+[[\s\w\.\/#]{3,4}]*) ([A-Za-z]{2}) ([A-Za-z]{1}\d{10,}) CARD (\d{4})$')
+    sub_df['transaction_date'] = sub_df.apply(format_date, axis=1)
     return sub_df
 
 def split_wf_dd(sub_df):
-	sub_df['transaction_type'] = 'Direct Deposit'
+	# sub_df['transaction_type'] = 'Direct Deposit'
 	sub_df[['formatted_description', 'transaction_date', 'source_id_num', 'recipient_name']] = sub_df['full_description'].str.extract(r'^([\w+\s*]+)DIRECT DEP (\d{6}) ([\w\d]+) ([\w\s,\.]+)$')
 
 	sub_df['transaction_date'] = sub_df['transaction_date'].apply(lambda x: dt.datetime.strptime(str(x), '%y%m%d'))
@@ -98,11 +99,20 @@ def split_wf_dd(sub_df):
 	sub_df = sub_df.drop('recipient_name',axis=1)
 	return sub_df
 
-	# print(desc)
-	# print(ufmt_dt)
-	# print(src_id)
-	# print(rcp_name)
-	# sub_df['transaction_date'] = dt.datetime.strptime(str(ufmt_dt), '%y%m%d')
-	# sub_df['formatted_description'] = f'{desc} {rcp_name}'
-	# sub_df['source_id_num'] = src_id
-	# return sub_df
+def format_date(row):
+
+	record_year = row['record_date'].year
+	record_mo = row['record_date'].month
+
+	tran_mo, tran_day = map(int, row['transaction_date'].split('/'))
+
+	if record_mo == tran_mo:
+		tran_year = record_year
+	elif record_mo < tran_mo:
+		tran_year = record_year - 1
+	elif record_mo > tran_mo:
+		tran_year = record_year
+
+	row['transaction_date'] = dt.datetime(tran_year, tran_mo, tran_day)
+
+	return row
